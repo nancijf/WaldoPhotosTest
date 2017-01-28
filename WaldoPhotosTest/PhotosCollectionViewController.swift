6db41b69e71b10546722fb90c0d5e601d0f4fcf0
Nanci Frank <me@nancifrank.com>
2017-01-28 15:53:42 -0500

    Added comments and cleaned up code

--- WaldoPhotosTest/PhotosCollectionViewController.swift
+++ WaldoPhotosTest/PhotosCollectionViewController.swift
@@ -37,7 +37,7 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
     }
     
     var minWidth: Int = 375
-    var imageCache = [String : UIImage]()
+    var imageCache = [String : UIImage]() // Used a dictionary for image cache. NSCache would clear when lost network connection
     
     let refreshControl = UIRefreshControl()
     
@@ -51,6 +51,7 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
         setupReachability()
     }
     
+    // Check if network is accessible; Alert user if it is not and set flag to stop downloading any more images
     func setupReachability() {
         reachability.whenReachable = { [unowned self] reachability in
             self.networkIsReachable = true
@@ -69,11 +70,14 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
         }
     }
     
+    // Setup Pull to Refresh
     func setupRefreshControl() {
         refreshControl.addTarget(self, action: #selector(refreshData(refreshControl:)), for: .valueChanged)
         collectionView?.addSubview(refreshControl)
     }
     
+    // Pull to Refresh: Clear all data and cache. Make new call to download images.
+    // Can refresh data after lose/regain network connection
     func refreshData(refreshControl: UIRefreshControl) {
         guard networkIsReachable else { return }
         imageCache.removeAll()
@@ -96,22 +100,30 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
-        self.imageCache.removeAll()
+        self.imageCache.removeAll() // Clear image cache if memory low
     }
     
     deinit {
         self.reachability.stopNotifier()
     }
     
+    // Check the width of the device to use for calculating photo size in collection view layout
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         minWidth = Int(view.frame.width)
         collectionView?.reloadData()
     }
     
+    /*
+     Set up configuration for Apollo to fetch data
+     Set loadingPhotos to true to stop retrieving more until completed
+     If no errors, retrieve URL for small2X for each record
+     Store URLs as strings in photoDataSource
+    */
     func fetchPhotoData() {
         guard networkIsReachable else {
             return
         }
+        
         loadingPhotos = true
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = ["Authorization": bearerToken]
@@ -125,6 +137,7 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
                 self.allPhotosLoaded = true
                 return
             }
+            
             if let data = data?.data {
                 if let photos = data.album?.photos?.records, photos.count > 0 {
                     if let title = data.album?.name {
@@ -140,11 +153,13 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
             } else {
                 self.allPhotosLoaded = true
             }
+            
             self.collectionView?.reloadData()
             self.loadingPhotos = false
         }
     }
     
+    // Infintite scrolling: Check if scrolled to bottom of images. Retrieve more data if needed
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard !loadingPhotos && !allPhotosLoaded && networkIsReachable else { return }
         
@@ -169,12 +184,17 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? PhotosCollectionViewCell
         
+        // Start indicator to show loading photos
         cell?.activityIndicator.startAnimating()
+        
+        // Check for data then if data is already stored in cache
         guard let url = photoDataSource[indexPath.item] else { return cell! }
         if let image = imageCache[url] {
             cell?.imageView.image = image
             cell?.activityIndicator.stopAnimating()
         } else {
+            
+            // Download image data and store in cache
             DispatchQueue.global().async {
                 if let tempImage: NSData = NSData(contentsOf: URL(string: url)!) {
                     let image: UIImage = UIImage(data: tempImage as Data)!
@@ -184,6 +204,8 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
                         cell?.imageView.image = image
                     })
                 } else {
+                    
+                // if image can't be downloaded, ude default image
                     self.imageCache[url] = UIImage(named: "BrokenImage")
                     DispatchQueue.main.async (execute: {
                         cell?.activityIndicator.stopAnimating()
@@ -192,13 +214,13 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
                 }
             }
         }
-
     
         return cell!
     }
  
     // MARK: UICollectionViewDelegateFlowLayout
 
+    // Set cell and image size for collection view. Calculate based on device size.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let itemSpacing = Int(flowLayout.minimumInteritemSpacing)
         let leftRightInset = Int(flowLayout.sectionInset.left + flowLayout.sectionInset.right)
@@ -210,13 +232,11 @@ class PhotosCollectionViewController: UICollectionViewController, UICollectionVi
 class PhotosCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var photoLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func prepareForReuse() {
         imageView.image = nil
         self.activityIndicator.stopAnimating()
-        photoLabel.text = ""
     }
     
 }
